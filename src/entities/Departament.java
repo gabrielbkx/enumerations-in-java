db8c8e14143af23808f51830cfc408f608bf4a30
Gabriel <oliveirafrerreira97@hotmail.com>
2024-08-11 17:04:47 -0300

    Criação do enum WorkerLeve , do pacote entities.enums

--- src/entities/Departament.java
+++ src/entities/Departament.java
@@ -1,5 +1,14 @@
+package entities;
+
 public class Departament {
     private String name;
+
+    public Departament() {
+    }
+
+    public Departament(String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name;
